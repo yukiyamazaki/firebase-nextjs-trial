a6d15437f442fbe9d1995abae5a26a01d8a0de44
YUKI YAMAZAKI <gooo0523@gmail.com>
2021-06-19 13:25:56 +0900

    update | firebaseの初期化とfirestoreとの接続

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,36 @@
 import Link from 'next/link';
 import Layout from '../components/Layout';
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Cてれててれれれれれれ 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>あああああああああああああああああああああ</a>
-      </Link>
-    </p>
-  </Layout>
-);
+import { db } from '../../firebase';
+
+const IndexPage = () => {
+  const handleClick = () => {
+    console.log('クリック');
+    db.collection('posts')
+      .add({
+        first: 'Ada',
+        last: 'Lovelace',
+        born: 1815,
+      })
+      .then((docRef) => {
+        console.log('Document written with ID: ', docRef.id);
+      })
+      .catch((error) => {
+        console.error('Error adding document: ', error);
+      });
+  };
+
+  return (
+    <Layout title="Home | Next.js + TypeScript Example">
+      <button onClick={handleClick}>テストfireStore</button>
+      <h1>Cてれててれれれれれれ 👋</h1>
+      <p>
+        <Link href="/about">
+          <a>あああああああああああああああああああああ</a>
+        </Link>
+      </p>
+    </Layout>
+  );
+};
 
 export default IndexPage;
