6633771e0b2e4da52582ee2a8c5b5d1a31e0d10b
YUKI YAMAZAKI <gooo0523@gmail.com>
2021-06-19 19:09:28 +0900

    test | DBの読み込み・書き込みテスト

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,15 @@
+import React, { useState } from 'react';
 import Link from 'next/link';
 import Layout from '../components/Layout';
 
 import { db } from '../../firebase';
 
 const IndexPage = () => {
-  const handleClick = () => {
-    console.log('クリック');
-    db.collection('posts')
+  const [testArray, setTestArray] = useState(null);
+
+  // 書き込み
+  const handleAddClick = () => {
+    db.collection('users')
       .add({
         first: 'Ada',
         last: 'Lovelace',
@@ -20,10 +23,39 @@ const IndexPage = () => {
       });
   };
 
+  // 書き込み２
+  const handleAdd2Click = () => {
+    db.collection('user')
+      .doc()
+      .collection('sakana')
+      .add({ first: 'abe', line: 'A', content: { test: 'aaaaa' } })
+      .then((docRef) => {
+        console.log(docRef);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  // 読み込み
+  const handleGetClick = () => {
+    db.collection('users')
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          console.log(`${doc.id} => ${doc.data().born}`);
+          // setTestArray(doc);
+        });
+      });
+  };
+
   return (
     <Layout title="Home | Next.js + TypeScript Example">
-      <button onClick={handleClick}>テストfireStore</button>
+      <button onClick={handleAddClick}>add-firebase</button>
+      <button onClick={handleAdd2Click}>add2-firebase</button>
+      <button onClick={handleGetClick}>get-firebase</button>
       <h1>Cてれててれれれれれれ 👋</h1>
+      <div>TEST:{testArray}</div>
       <p>
         <Link href="/about">
           <a>あああああああああああああああああああああ</a>
